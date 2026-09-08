58c3193fa9727e0aacc7c60abb329e0f28aa55a0
eltom1 <tomasdellosso4@gmail.com>
2026-09-08 00:17:31 -0300

    Cli

---
+++ cli/table.go
@@ -0,0 +1,26 @@
+
+
+
+package UITable
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	baseStyle = lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		Foreground(lipgloss.Color("250"))
+
+)
+
+type model struct {
+	table table.Model
+}
+
